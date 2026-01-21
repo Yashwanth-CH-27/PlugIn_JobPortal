e037edc6076d8a2ef8a9112c80d783cd39889aee
Yash <yaswanth.chundru@gmail.com>
2026-01-21 12:31:41 +0530

    Defined Application Schema

---
+++ Backend/src/models/Application.js
@@ -0,0 +1,37 @@
+const mongoose = require("mongoose")
+
+const applicationSchema = new mongoose.Schema(
+    {
+        jobId:{
+            type: mongoose.Schema.Types.ObjectId,
+            ref: "job",
+            required: true,
+        },
+        applicationId:{
+            type: mongoose.Schema.Types.ObjectId,
+            ref: "User",
+            required: true,
+        },
+        resumeUrl:{
+            type: String,
+            required: true,
+        },
+        status:{
+            type: String,
+            enum: ["applied", "reviewed", "accepted", "rejected"],
+            default: "applied",
+        },
+        appliedAt:{
+            type: Date,
+            default: Date.now,
+        }
+    },
+    {timestamps: true}
+)
+
+applicationSchema.index(
+    {jobId: 1, applicationId: 1},
+    {unique: true}
+)
+
+module.exports = mongoose.model("Application", applicationSchema)
