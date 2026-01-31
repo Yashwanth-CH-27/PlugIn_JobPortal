2203c9ff2e6e224d532804e5baff1a125bb1a542
Yash <yaswanth.chundru@gmail.com>
2026-01-31 10:47:51 +0530

    Bug fix of path in main.jsx

--- Frontend/src/main.jsx
+++ Frontend/src/main.jsx
@@ -2,7 +2,7 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { RouterProvider } from "react-router-dom";
 import { router } from "./routes/router";
-import { AuthProvider } from "./context/AuthContext";
+import { AuthProvider } from "./context/AuthProvider";
 import "./index.css";
 
 createRoot(document.getElementById("root")).render(
