8279b91d39afee61b1053b03fe3b453cb36aacc3
Yash <yaswanth.chundru@gmail.com>
2026-02-08 14:58:58 +0530

    Implement Job details component

--- Frontend/src/pages/jobs/JobDetails.jsx
+++ Frontend/src/pages/jobs/JobDetails.jsx
@@ -1,9 +1,81 @@
-import React from 'react'
+import React, { useEffect, useState } from "react";
+import { useNavigate, useParams } from "react-router-dom";
+import { useAuth } from "../../customHooks/useAuth";
+import { getJobInfo } from "../../api/jobseekerApi";
+import toast from "react-hot-toast";
+import { applyJob } from "../../api/applicationApi";
 
 const JobDetails = () => {
-  return (
-    <div>JobDetails</div>
-  )
-}
+  const { jobId } = useParams();
+  const { user } = useAuth();
+  const [job, setJob] = useState(null);
+  const [loading, setLoading] = useState(true);
+  const [applyLoading, setApplyLoading] = useState(false);
+  const navigate = useNavigate();
 
-export default JobDetails
+  useEffect(() => {
+    const fetchJob = async () => {
+      try {
+        setLoading(true);
+        const res = await getJobInfo(jobId);
+        setJob(res.data.job || res.data);
+      } catch (err) {
+        toast.error(err?.response?.data?.message || "Failed to Load Job");
+      } finally {
+        setLoading(false);
+      }
+    };
+    fetchJob();
+  }, [jobId]);
+
+  const handleApply = async () => {
+    if (!user) {
+      toast.error("Please Login to apply!");
+      navigate("/login");
+      return;
+    }
+
+    if (user.role !== "jobseeker") {
+      toast.error("Only jobseekers can apply for jobs!");
+      return;
+    }
+
+    try {
+      applyLoading(true);
+      await applyJob(jobId);
+      toast.success("Applied Successfully!");
+    } catch (err) {
+      toast.error(err?.response?.data?.message || "Apply Failed");
+    } finally {
+      setApplyLoading(false);
+    }
+  };
+
+  if (loading) return <p>Loading Job Details...</p>;
+  if (!job) return <p>No Details found...</p>;
+
+  return (
+    <div style={{ padding: "20px" }}>
+      <h1>{job.title}</h1>
+      <p>
+        <b>Company:</b> {job.companyName}
+      </p>
+      <p>
+        <b>Location:</b> {job.location}
+      </p>
+
+      <h3>Description</h3>
+      <p>{job.description}</p>
+
+      <p><b>JobType:</b>{job.jobType}</p>
+      <p><b>Salary:</b>{job.salaryRange}</p>
+      <p><b>Location:</b>{job.location}</p>
+
+      <button disabled={applyLoading} onClick={handleApply}>
+        {applyLoading ? "Applying..." : "Apply Job"}
+      </button>
+    </div>
+  );
+};
+
+export default JobDetails;
